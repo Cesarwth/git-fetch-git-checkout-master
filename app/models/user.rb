f2bc9bde4b3eb0239e339232b6e124a6663ea84b
miguetv17 <miguetv17@gmail.com>
2017-06-27 22:35:36 -0500

    admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :roles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    self.roles.where(:name => 'admin').count > 0
+  end
 end
