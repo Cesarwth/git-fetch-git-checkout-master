a7ae6b2944f094e2d5a531d340b6664100460903
miguetv17 <miguetv17@gmail.com>
2017-07-09 21:43:34 -0500

    seeds for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,10 @@
 role = Role.create!(name: 'admin', status: 1)
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', roles: [role])
 # Creates default unique contact record
-# ["id", "address", "phone", "email", "status", "created_at", "updated_at"]
 Contact.create(address: 'Sector Capelo Calle Cipreses. ref:Frente a la Urbanización Hacienda Capelo', phone: 'Administración: 022863322 ; Garita: 022867407', email: 'ciudadelaterranova@yahoo.com')
+# Creates default categories
+Category.create(title: 'Información Organizacional', description: 'Información Organizacional texto', position: 0, image: 'informacion_organizacional.png', status: 1)
+Category.create(title: 'Información Administrativa', description: 'Información Administrativa texto', position: 1, image: 'informacion_administrativa.png', status: 1)
+Category.create(title: 'Información Financiera', description: 'Información Financiera texto', position: 2, image: 'informacion_financiera.png', status: 1)
+Category.create(title: 'Eventos Sociales', description: 'Eventos Sociales Texto', position: 3, image: 'eventos_sociales.png', status: 1)
+Category.create(title: 'Sede Social - BBQ - Canchas Deportivas', description: 'Sede Social - BBQ - Canchas Deportivas texto', position: 4, image: 'sede_social.png', status: 1)
