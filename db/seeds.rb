74cc206e2fed1a2bf9c7cc2910674b78e20100a7
miguetv17 <miguetv17@gmail.com>
2017-07-09 19:31:31 -0500

    fix contact CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 role = Role.create!(name: 'admin', status: 1)
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', roles:[role])
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', roles: [role])
+# Creates default unique contact record
+# ["id", "address", "phone", "email", "status", "created_at", "updated_at"]
+Contact.create(address: 'Test Address', phone: '1234567890', email: 'test@email.com')
