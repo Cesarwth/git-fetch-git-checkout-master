79f4006d808d2c839c0f86e1e102327401a8a7fe
cesarwth@gmail.com <Alisson17>
2017-08-13 23:11:38 -0500

    Update seed Information

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,13 @@ User.create!(email: 'manager@example.com', password: 'password', password_confir
 # Creates default unique contact record
 Contact.create(address: 'Sector Capelo Calle Cipreses. ref:Frente a la Urbanización Hacienda Capelo', phone: 'Administración: 022863322 ; Garita: 022867407', email: 'ciudadelaterranova@yahoo.com')
 # Creates default categories
-Category.create(title: 'Información Organizacional', description: 'Información Organizacional texto', position: 0, image: File.new("#{Rails.root}/app/assets/images/default_categories/informacion_organizacional.jpg"), status: 1)
-Category.create(title: 'Información Administrativa', description: 'Información Administrativa texto', position: 1, image: File.new("#{Rails.root}/app/assets/images/default_categories/informacion_administrativa.jpg"), status: 1)
-Category.create(title: 'Información Financiera', description: 'Información Financiera texto', position: 2, image: File.new("#{Rails.root}/app/assets/images/default_categories/informacion_financiera.jpg"), status: 1)
-Category.create(title: 'Eventos Sociales', description: 'Eventos Sociales Texto', position: 3, image: File.new("#{Rails.root}/app/assets/images/default_categories/eventos_sociales.jpg"), status: 1)
-Category.create(title: 'Sede Social - BBQ - Canchas Deportivas', description: 'Sede Social - BBQ - Canchas Deportivas texto', position: 4, image: File.new("#{Rails.root}/app/assets/images/default_categories/sede_social.jpg"), status: 1)
+Category.create(title: 'Información Organizacional', description: 'Información Organizacional texto', position: 1, image: File.new("#{Rails.root}/app/assets/images/default_categories/informacion_organizacional.jpg"), status: 1)
+Category.create(title: 'Información Administrativa', description: 'Información Administrativa texto', position: 2, image: File.new("#{Rails.root}/app/assets/images/default_categories/informacion_administrativa.jpg"), status: 1)
+Category.create(title: 'Información Financiera', description: 'Información Financiera texto', position: 3, image: File.new("#{Rails.root}/app/assets/images/default_categories/informacion_financiera.jpg"), status: 1)
+Category.create(title: 'Eventos Sociales', description: 'Eventos Sociales Texto', position: 4, image: File.new("#{Rails.root}/app/assets/images/default_categories/eventos_sociales.jpg"), status: 1)
+Category.create(title: 'Sede Social - BBQ - Canchas Deportivas', description: 'Sede Social - BBQ - Canchas Deportivas texto', position: 5, image: File.new("#{Rails.root}/app/assets/images/default_categories/sede_social.jpg"), status: 1)
 
 RotatingImage.create(name: 'Imagen1', file_name: '/assets/rotativas/imagen1.jpg',  position: 1 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen1.jpg"))
-RotatingImage.create(name: 'Imagen2', file_name: '/assets/rotativas/imagen2.jpg',  position: 1 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen2rails.jpg"))
-RotatingImage.create(name: 'Imagen3', file_name: '/assets/rotativas/imagen3.jpg',  position: 1 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen3.jpg"))
-RotatingImage.create(name: 'Imagen4', file_name: '/assets/rotativas/imagen4.jpg',  position: 1 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen4.jpg"))
+RotatingImage.create(name: 'Imagen2', file_name: '/assets/rotativas/imagen2.jpg',  position: 2 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen2rails.jpg"))
+RotatingImage.create(name: 'Imagen3', file_name: '/assets/rotativas/imagen3.jpg',  position: 3 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen3.jpg"))
+RotatingImage.create(name: 'Imagen4', file_name: '/assets/rotativas/imagen4.jpg',  position: 4 , status: 1,rotating_file: File.new("#{Rails.root}/app/assets/images/rotativas/imagen4.jpg"))
