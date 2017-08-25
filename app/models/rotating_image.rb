76975f342165080ed64e3d1b2fe87115caeb5a48
FabricioH2 <fabricioph2@gmail.com>
2017-08-24 23:00:40 -0500

    Users

--- app/models/rotating_image.rb
+++ app/models/rotating_image.rb
@@ -1,5 +1,5 @@
 class RotatingImage < ApplicationRecord
-  has_attached_file :rotating_file, styles: { medium: '500x500>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  has_attached_file :rotating_file, styles: { medium: '500x500>', thumb: '100x100>' , full: '2116x1500>'}, default_url: '/images/:style/missing.png'
   validates_attachment :rotating_file,
                        presence: true,
                        content_type: { content_type: %w[image/jpeg image/png] }
