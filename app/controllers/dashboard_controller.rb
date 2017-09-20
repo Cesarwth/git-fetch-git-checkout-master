7e654c919e664768c0787f08e08d7af038932483
miguetv17 <miguetv17@gmail.com>
2017-09-20 00:36:49 -0500

    #2 Send email to manager

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,6 +15,7 @@ class DashboardController < ApplicationController
       if name && email
         # Tell the UserMailer to send a welcome email after save
         ContactMailer.contact_email(name, email).deliver_later
+        ContactMailer.manager_email(params.as_json).deliver_later
         format.html
         format.js
       end
