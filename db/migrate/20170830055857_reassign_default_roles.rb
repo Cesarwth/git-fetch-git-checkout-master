a50b8f73837680a2f86181c95cf3921873321ca1
miguetv17 <miguetv17@gmail.com>
2017-10-17 22:01:48 -0500

    Validate nil on roles migration

--- db/migrate/20170830055857_reassign_default_roles.rb
+++ db/migrate/20170830055857_reassign_default_roles.rb
@@ -2,11 +2,15 @@ class ReassignDefaultRoles < ActiveRecord::Migration[5.1]
   def change
     default_admin = User.where(email: 'admin@example.com').first
     admin_role = Role.where(name: 'admin').first
-    default_admin.role = admin_role
-    default_admin.save!
+    if default_admin
+      default_admin.role = admin_role
+      default_admin.save!
+    end
     default_manager = User.where(email: 'manager@example.com').first
     manager_role = Role.where(name: 'manager').first
-    default_manager.role = manager_role
-    default_manager.save!
+    if default_manager
+      default_manager.role = manager_role
+      default_manager.save!
+    end
   end
 end
